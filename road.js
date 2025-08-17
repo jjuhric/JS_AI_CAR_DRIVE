23e6dda86a02f4c750805aeac9f8cba04a9a78eb
Jeff Uhrick <jjuhric@gmail.com>
2025-08-16 21:21:09 -0500

    adding dashes to road and finding lane centers

--- road.js
+++ road.js
@@ -1,6 +1,7 @@
 class Road {
     constructor(x, width, laneCount = 3) {
         this.x = x;
+
         this.width = width;
         this.laneCount = laneCount;
 
@@ -13,12 +14,23 @@ class Road {
 
     }
 
+    getCenterOfLane(laneIndex) {
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount - 1) * laneWidth;
+    }
+
     draw(ctx) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
         for(let i=0; i <= this.laneCount; i++) {
             const x = linearInterpolation(this.left, this.right, i / this.laneCount);
+            if(i > 0 && i < this.laneCount) {
+                ctx.setLineDash([20, 20]);
+            }
+            else {
+                ctx.setLineDash([]);
+            }
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
